f727c682a224a3c54e2e1a1cb6149458f3af77a0
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:21:10 -0400

    added package info header-only flag

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,26 @@ class BoostMplConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/mpl"
-    description = "For a description of this library, please visit http://boost.org/mpl "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "mpl"
-
+    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Predef/1.64.0@bincrafters/testing", \
+                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
+                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing",\
+                      "Boost.Utility/1.64.0@bincrafters/testing"
+                      
+                      #config0 core2 predef0 preprocessor0 static_assert1 type_traits3 utility5
+    
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
