e0b399af15ed8028419c6c4bfeb03fd3be7e024c
Jerry W <jerrywiltse@gmail.com>
2017-09-20 02:19:06 -0400

    fixed erroneous recipes for group members

--- conanfile.py
+++ conanfile.py
@@ -5,31 +5,17 @@ class BoostMplConan(ConanFile):
     version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-mpl"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_names = ["iterator"]
-    requires =  "Boost.Config/1.65.1@bincrafters/testing", \
-                      "Boost.Core/1.65.1@bincrafters/testing", \
-                      "Boost.Predef/1.65.1@bincrafters/testing", \
-                      "Boost.Preprocessor/1.65.1@bincrafters/testing", \
-                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-                      "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-                      "Boost.Utility/1.65.1@bincrafters/testing"
-                      
-                      #config core predef preprocessor static_assert type_traits utility5
-    
-    def source(self):
-        boostorg_github = "https://github.com/boostorg"
-        archive_name = "boost-" + self.version  
-        for lib_short_name in self.lib_short_names:
-            tools.get("{0}/{1}/archive/{2}.tar.gz"
-                .format(boostorg_github, lib_short_name, archive_name))
-            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
-
-    def package(self):
-        for lib_short_name in self.lib_short_names:
-            include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)
-
+    requires =  "Boost.Level5Group/1.65.1@bincrafters/testing"
+   
     def package_id(self):
-        self.info.header_only() 
+        self.info.header_only()
+        
+    #This library is part of one or more cyclic dependency groups within Boost.
+    
+    #All members of cyclic dependency groups must be built under single package per group for Conan.
+    
+    #The combination is performed in the package(s) listed in the requires field.
+    
+    #This package enables simple consumption of this library while abstracting away the cyclic dependency issues. 
