e70add669271303028ac549ab1c0f1cf4555a9cd
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostMplConan(base.BoostBaseConan):
     name = "boost_mpl"
@@ -20,5 +20,3 @@ class BoostMplConan(base.BoostBaseConan):
         "boost_type_traits",
         "boost_utility"
     ]
-
-
