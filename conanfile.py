7c055e2be24d913624130b1c22d63487c338244f
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:38:07 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostMplConan(base.BoostBaseConan):
     name = "boost_mpl"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_mpl"
     lib_short_names = ["mpl"]
     header_only_libs = ["mpl"]
