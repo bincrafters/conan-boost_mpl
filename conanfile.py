b6c2189ae123154a47c4026095823833e0e12933
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:06 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,20 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostMplConan(base.BoostBaseConan):
     name = "boost_mpl"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_mpl"
-    lib_short_names = ["mpl"]
-    header_only_libs = ["mpl"]
-    b2_requires = [
-        "boost_config",
-        "boost_core",
-        "boost_predef",
-        "boost_preprocessor",
-        "boost_static_assert",
-        "boost_type_traits",
-        "boost_utility"
-    ]
+    version = "1.70.0"
